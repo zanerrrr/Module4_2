b30274bd6c91de362b08d556b8c8f8746687bdfb
zanerrrr <zanerubene1995@gmail.com>
2021-10-20 00:41:46 +0300

    final

---
+++ Module4_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Module4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double rad;
+            Console.WriteLine("Calculation of a circle area!");
+            Console.WriteLine("");
+            Console.WriteLine("Enter the radius of your circle: ");
+            rad = double.Parse(Console.ReadLine());
+            //double fromFunction = Circle(rad);
+            Console.WriteLine($"The Area of your Circle is {Circle(rad)}");
+            Console.WriteLine("------------------------------------------------------");
+            double side1;
+            double side2;
+            double area;
+            Console.WriteLine("\nCalculation of a square area!");
+            Console.WriteLine("");
+            Console.WriteLine("Enter the measurements of one of the square sides: ");
+            side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the measurements of the other square side: ");
+            side2 = double.Parse(Console.ReadLine());
+            area = Square(side1, side2);
+            Console.WriteLine($"The area of your sqare is {area}");
+            Console.ReadLine();
+
+        }
+        static double Circle(double rad)
+        {
+            return rad = Math.PI * rad * rad;
+            //
+            //Console.ReadLine();
+        }
+        static double Square(double side1, double side2)
+        {
+            return side1 * side2;
+            //
+            //Console.ReadLine();
+        }
+    }
+}
